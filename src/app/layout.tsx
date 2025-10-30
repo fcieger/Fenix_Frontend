7f42bf01933d3ce2384b2c8d111f8beb8eeeb34e
fcieger <fcieger1982@gmail.com>
2025-10-30 03:18:30 -0300

    chore(src/app): minor update in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/contexts/auth-context";
+import { FeedbackProvider } from "@/contexts/feedback-context";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,11 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning={true}
       >
-        <AuthProvider>{children}</AuthProvider>
+        <AuthProvider>
+          <FeedbackProvider>
+            {children}
+          </FeedbackProvider>
+        </AuthProvider>
       </body>
     </html>
   );
