d6779419c8e6198b05fd3347272b8d48521948e2
Leonam Moreira Ribeiro <liru@Leonams-MacBook-Pro.local>
2025-12-11 09:39:24 -0300

    feat(components/shared/Modals): created index

---
+++ src/components/shared/Modals/index.ts
@@ -0,0 +1,8 @@
+export { Modal } from "./Modal";
+export type { ModalProps } from "./Modal";
+
+export { ConfirmModal } from "./ConfirmModal";
+export type { ConfirmModalProps, ConfirmModalVariant } from "./ConfirmModal";
+
+export { FormModal } from "./FormModal";
+export type { FormModalProps } from "./FormModal";
